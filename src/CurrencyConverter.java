e3e31ad0f9659bf1be08a5d84ced1bf2b4a9bbc2
Anton Okhrimenko <antony.okhrimenko@gmail.com>
2024-07-09 21:06:57 +0200

    1st commit

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class CurrencyConverter {
+
+// Класс, который выполняет операции обмена валют, используя предоставленные
+// курсы обмена
+// и сохраняет историю обменов после каждой транзакции (в идеале)
+
+public class CurrencyConverter {
+
+        private final ExchangeRate rateProvider;
+
+        // конструктор
+        public CurrencyConverter() {
+            this.rateProvider = new ExchangeRate();
+        }
+
+        // метод для обмена валюты (вводим сумму и пару откуда и куда)
+        public double exchange(double amount, String fromCurrency, String toCurrency) {
+            double rate = rateProvider.getExchangeRate(fromCurrency, toCurrency);
+            // double result = amount * rate;
+            return (double) amount * rate;
+
+
+            // еще тут нужно хранить историю где-то транзакции на будущее
+        }
+
 }
