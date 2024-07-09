559b6bf72653d51e681cb443f7152998b7e04372
umiadore <umi.adore@gmail.com>
2024-07-10 00:07:36 +0400

    salam

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
 
@@ -6,4 +8,23 @@ public class Main {
     // введите сумма
     // введите валюту
     // введите конечную валюту
+    public static void main(String[] args) {
+        CurrencyConverter  currencyConverter = new CurrencyConverter();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введите сумму для обмена: ");
+            double amount = scanner.nextDouble();
+            scanner.nextLine();
+            System.out.println("Введите валюту, которую хотите обменять: ");
+            String fromCurrency = scanner.nextLine().toUpperCase();
+
+            System.out.println("Введите валюту, на которую хотите обменять: ");
+            String toCurrency = scanner.nextLine().toUpperCase();
+
+            double result = currencyConverter.exchange(amount, fromCurrency,toCurrency);
+
+            System.out.println("Конечная сумма: " + result + " " +  toCurrency);
+        }
+    }
 }
