ca8b2519fb632a836df38caac2830196bf318cc8
nadja <nadja.kalenda31@gmail.com>
2024-07-11 19:15:02 +0200

    yyyy

---
+++ src/TransactionRecord.java
@@ -0,0 +1,30 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.chrono.ChronoLocalDateTime;
+
+public class TransactionRecord {
+    private final LocalDateTime dateTime;
+    private final  double amount;
+    private final  String fromCurrency;
+    private final String toCurrency;
+    private final double result;
+
+    public TransactionRecord(double amount, String fromCurrency, String toCurrency, double result) {
+        this.dateTime = LocalDateTime.now();
+        this.amount = amount;
+        this.fromCurrency = fromCurrency;
+        this.toCurrency = toCurrency;
+        this.result = result;
+    }
+
+    @Override
+    public String toString() {
+        return "TransactionRecord{" +
+                "dateTime=" + dateTime +
+                ", amount=" + amount +
+                ", fromCurrency='" + fromCurrency + '\'' +
+                ", toCurrency='" + toCurrency + '\'' +
+                ", result=" + result +
+                '}';
+    }
+}
--- src/TransactionsHistory.java
+++ src/TransactionsHistory.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TransactionsHistory {
-}
+
+   private final List<TransactionRecord> records;
+
+    public TransactionsHistory(List<TransactionRecord> records) {
+        this.records = new ArrayList<>();
+    }
+
+    public void addRecord(TransactionRecord record){
+        records.add(record);
+    }
+    public void printHistory(){
+        for (TransactionRecord record:records) {
+            System.out.println(record);
+
+        }
+
+        }
+    }
+
